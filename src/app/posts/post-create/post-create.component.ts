67d1c8c9ddf8a02ce8ee9dea8c871dcafef0c33b
Colby Taperts <colbytaperts@gmail.com>
2019-06-29 22:53:33 -0500

    add two way binding

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -5,5 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './post-create.component.html'
 })
 export class PostCreateComponent {
-
+  enteredValue = '';
+  newPost = 'No content';
+  onAddPost(postInput: HTMLTextAreaElement) {
+    // console.log(postInput.value);
+    // console.dir(postInput.value);
+    // this.newPost = postInput.value;
+    this.newPost = this.enteredValue;
+  }
 }
