5bd6e13b7a12452b00f4609b34df0314b7438c60
Colby Taperts <colbytaperts@gmail.com>
2019-07-06 22:13:00 -0500

    set the result back

--- backend/app.js
+++ backend/app.js
@@ -53,6 +53,7 @@ app.get('/api/posts', (req, res) => {
 app.delete('/api/posts/:id', (req, res) => {
   Post.deleteOne({ _id: req.params.id})
     .then(result => {
+      console.log(result);
       res.status(200).json({ message: 'Post deleted' });
     });
 });
