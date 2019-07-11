8d20adfe0cd84a5896b8120499b7c18485e5f2c6
Colby Taperts <colbytaperts@gmail.com>
2019-07-10 21:06:49 -0500

    init paginator

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PageEvent } from '@angular/material'
 import { Subscription } from 'rxjs';
 
 import { Post } from '../post.model';
@@ -13,6 +14,9 @@ export class PostListComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
   private postsSub: Subscription;
   isLoading = false;
+  totalPosts = 10;
+  postsPerPage = 2;
+  pageSizeOptions = [1, 2, 5, 10];
 
   constructor(public postsService: PostsService) {
   }
@@ -25,6 +29,10 @@ export class PostListComponent implements OnInit, OnDestroy {
         this.posts = posts;
       });
     this.isLoading = false;
+  }
+
+  onChangedPage(pageData: PageEvent) {
+    console.log(pageData);
   }
 
   onDelete(postId: string) {
