8319fbdf7597b57c95169881a699a94c71e104e1
Colby Taperts <colbytaperts@gmail.com>
2019-07-13 22:39:29 -0500

    init login api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const UserModel = require('../models/user');
 
@@ -24,6 +25,38 @@ router.post('/signup', (req, res) => {
             error: error
           });
         });
+    });
+});
+
+router.post('/login', (req, res) => {
+  let user;
+  UserModel.findOne({email: req.body.email})
+    .then(userData => {
+      if (!userData) {
+        return res.status(401).json({
+          message: 'Auth failed'
+        });
+      }
+      user = userData;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if (!result) {
+        res.status(401).json({
+          message: 'Auth failed'
+        });
+      }
+      const token = jwt.sign(
+        {email: user.email, userId: user._id},
+        'windowswallowknowarrow',
+        {expiresIn: '1h'}
+      );
+    })
+    .catch(error => {
+      res.status(401).json({
+        message: 'Auth failed',
+        error: error
+      });
     });
 });
 
